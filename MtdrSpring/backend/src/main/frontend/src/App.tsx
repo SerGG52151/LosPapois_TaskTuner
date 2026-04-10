a5c481585ca24037a9371e7870f3b6d07519d745
BrunoRiv73 <a01571663@tec.mx>
2026-04-09 19:34:43 -0600

    Added LoginPage.tsx with basic linkage between signup and login

--- MtdrSpring/backend/src/main/frontend/src/App.tsx
+++ MtdrSpring/backend/src/main/frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import TasksPage from './Pages/TasksPage';
 import LoginPage from './Pages/LoginPage';
+import SignupPage from './Pages/SignupPage'; 
 import TailwindTestPage from './Pages/TailwindTestPage';
 
 function App() {
@@ -10,11 +11,13 @@ function App() {
       <nav style={{ padding: '0.5rem' }}>
         <Link to="/login">Home</Link> {' | '}
         <Link to="/tasks">Tasks</Link> {' | '}
+        <Link to="/signup">Signup</Link> {' | '}
         <Link to="/tailwind-test" className="text-blue-500 hover:text-blue-700 font-semibold underline">Tailwind Test</Link>
       </nav>
       <Routes>
         <Route path="/login" element={<LoginPage/>} />
         <Route path="/tasks" element={<TasksPage/>} />
+        <Route path="/signup" element={<SignupPage/>} />
         <Route path="/tailwind-test" element={<TailwindTestPage/>} />
       </Routes>
     </div>
